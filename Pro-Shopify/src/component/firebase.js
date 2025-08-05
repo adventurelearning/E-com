5d8473f73777001a4f6f5269e4f10968e818b0a8
Adventureexamrally <adventure.webapp@gmail.com>
2025-08-05 15:46:26 +0530

    update oauth

--- Pro-Shopify/src/component/firebase.js
+++ Pro-Shopify/src/component/firebase.js
@@ -6,14 +6,24 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDnjEscV2UI_c5pQVoMxhuryTkW9n0jQWU",
+//   authDomain: "pro-shopify.firebaseapp.com",
+//   projectId: "pro-shopify",
+//   storageBucket: "pro-shopify.firebasestorage.app",
+//   messagingSenderId: "838195451103",
+//   appId: "1:838195451103:web:35f2a0a9c0c6d7b621856a",
+//   measurementId: "G-7RJTJE75PR"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDnjEscV2UI_c5pQVoMxhuryTkW9n0jQWU",
-  authDomain: "pro-shopify.firebaseapp.com",
-  projectId: "pro-shopify",
-  storageBucket: "pro-shopify.firebasestorage.app",
-  messagingSenderId: "838195451103",
-  appId: "1:838195451103:web:35f2a0a9c0c6d7b621856a",
-  measurementId: "G-7RJTJE75PR"
+  apiKey: "AIzaSyAcOqKl0k4fWYWrlsm7YyO0AOsjCEBir2A",
+  authDomain: "adventure-apps.firebaseapp.com",
+  projectId: "adventure-apps",
+  storageBucket: "adventure-apps.firebasestorage.app",
+  messagingSenderId: "319953650139",
+  appId: "1:319953650139:web:d42dd5d2bdfa82445727fe",
+  measurementId: "G-69MX19KL8E"
 };
 
 // Initialize Firebase
